adb18af87c78a7cc428cb59eff216aeb78106392
Terran Blake <terranblake@gmail.com>
2025-03-21 13:53:34 -0500

    chore(setup): first platformio build

--- components/common/scripts/component_builder.py
+++ components/common/scripts/component_builder.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 import os
 import sys
+from SCons.Script import DefaultEnvironment
 
-# This script helps PlatformIO build the correct component
-# It's called by the extra_scripts setting in platformio.ini
+# Get PlatformIO build environment
+env = DefaultEnvironment()
 
-component = os.environ.get('COMPONENT', None)
-if not component:
-    print('Error: COMPONENT environment variable not set')
+# Access the build flag defined in platformio.ini
+component_type = env.GetProjectOption("build_flags")
+
+# Filter for the COMPONENT_TYPE value
+component_type_value = None
+for flag in component_type:
+    if flag.startswith("-D COMPONENT_TYPE="):
+        component_type_value = flag.split("=")[1]
+        break
+
+if not component_type_value:
+    print("Error: COMPONENT_TYPE build flag not found")
     sys.exit(1)
 
-print(f'Building component: {component}')
+custom_component = env.GetProjectOption("custom_component")
+print(f"Building component type: {component_type_value} for component: {custom_component}")
